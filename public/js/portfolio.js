8a345bd5e800d71a78387c9f41dae74e7ae82332
Michael Olie <aolie94@gmail.com>
2018-04-13 18:25:30 -0700

    credited preloader section to fadeout once all assets have loaded

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function($){
 	var Loader = document.getElementById('preloader');
 
 	window.addEventListener('load', function(){
-		$(Loader).css({'display': 'none' })
+		$(Loader).fadeOut();
 	});
 
 });
